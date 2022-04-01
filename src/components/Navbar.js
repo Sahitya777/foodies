ac94037dc12d39b532ce3e0156078b8582cc7a2c
Sahitya77 <sahityanijhawan@gmail.com>
2022-04-01 18:46:04 +0530

    Foodies Corner!!

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+function Navbar(){
+    return(
+        <div className="navbar container">
+            <a href="#!" className="logo">F<span>oo</span>dies Corner</a>
+            <div className="nav-links">
+                <a href="#!">Home</a>
+                <a href="#!">Recipes</a>
+                <a href="#!">Settings</a>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
