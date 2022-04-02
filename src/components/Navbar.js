2586c62edddbd57d34539b3e7b0309656eb6bc9e
Sahitya77 <sahityanijhawan@gmail.com>
2022-04-02 13:14:24 +0530

    navbar updated!!

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,43 @@
+import {useState} from 'react'
+import Sidebar from './Sidebar';
 function Navbar(){
+    const links=[
+        {
+            name:"Home",
+            path:"/"
+        },
+        {
+            name:"Recipes",
+            path:"/recipes"
+        },
+        {
+            name:"Settings",
+            path:"/settings"
+        }
+    ]
+    const [showSidebar,setShowSidebar]=useState(false)
+
     return(
-        <div className="navbar container">
-            <a href="#!" className="logo">F<span>oo</span>dies Corner</a>
-            <div className="nav-links">
-                <a href="#!">Home</a>
-                <a href="#!">Recipes</a>
-                <a href="#!">Settings</a>
+        <div>
+            <div className="navbar container">
+                <a href="#!" className="logo">F<span>oo</span>dies Corner</a>
+                <div className="nav-links">
+                { links.map(link => (
+                        <a href='#!' key={link.name}>{link.name}</a>
+                    )) }
+                    {/* <a href="#!">Home</a>
+                    <a href="#!">Recipes</a>
+                    <a href="#!">Settings</a> */}
+                </div>
+                <div onClick={()=> setShowSidebar(!showSidebar)} className={showSidebar ? "sidebar-btn active":"sidebar-btn"}>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                </div>
             </div>
+            <Sidebar links={links}/>            
         </div>
+        
     )
 }
 
