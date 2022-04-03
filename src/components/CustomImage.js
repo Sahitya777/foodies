65defc9087bfa47911b738b129fccc1fc1ade25d
Sahitya77 <sahityanijhawan@gmail.com>
2022-04-03 15:57:57 +0530

    Hero section complete!!

--- src/components/CustomImage.js
+++ src/components/CustomImage.js
@@ -1,9 +1,10 @@
 
 
-function CustomImage({src,pt}) {
+function CustomImage({imgSrc,pt}) {
   return (
     <div className="custom-image" style={{paddingTop: pt}}>
-        CustomImage
+        
+        <img src={imgSrc} alt=""/>
     </div>
   )
 }
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,23 +1,29 @@
 import CustomImage from "./CustomImage"
 
-function HeroSection() {
-
-    const images=[
-        
+export default function HeroSection(){
+    const images = [
+        "/img/gallery/img_1.jpg",
+        "/img/gallery/img_2.jpg",
+        "/img/gallery/img_3.jpg",
+        "/img/gallery/img_4.jpg",
+        "/img/gallery/img_5.jpg",
+        "/img/gallery/img_6.jpg",
+        "/img/gallery/img_7.jpg",
+        "/img/gallery/img_8.jpg",
+        "/img/gallery/img_9.jpg"
     ]
-
-  return (
-    <div className="section hero">
-        <div className="col">
-            <h1 className="title">What Are We <br></br>About</h1>
-            <p className="info">FoodiesCorner is a place where you can please your soul and tummy with delicious food recepies of all cuisine And our service is absolutely free.So start exploring now.</p>
-            <button className="btn">explore now</button>
+    return (
+        <div className="section hero">
+            <div className="col typography">
+                <h1 className="title">What Are We <br></br> About</h1>
+                <p className="info">FoodiesHub is a place where you can please your soul and tummy with delicious food recepies of all cuisine. And our service is absolutely free. So start exploring now.</p>
+                <button className="btn">explore now</button>
+            </div>
+            <div className="col gallery">
+                { images.map((src, index) => (
+                    <CustomImage key={index} imgSrc={src} pt={"90%"} />
+                )) }
+            </div>
         </div>
-        <div className="col gallery">
-            <CustomImage pt={"85%"}/>
-        </div>
-    </div>
-  )
+    )
 }
-
-export default HeroSection
