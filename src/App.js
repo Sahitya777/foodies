f077c761393b19675ce30a2d78ee62e1c89c62ef
Sahitya77 <sahityanijhawan@gmail.com>
2022-04-03 15:34:53 +0530

    images

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <div className="container main">
+        <HeroSection />
+      </div>
     </div>
   );
 }
---
+++ src/components/CustomImage.js
@@ -0,0 +1,11 @@
+
+
+function CustomImage({src,pt}) {
+  return (
+    <div className="custom-image" style={{paddingTop: pt}}>
+        CustomImage
+    </div>
+  )
+}
+
+export default CustomImage
