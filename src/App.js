553beaaa3d03497916dafc97f12be5c9a0737718
Sahitya77 <sahityanijhawan@gmail.com>
2022-04-04 18:07:34 +0530

    improve section completed!!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
+import ImproveSkills from "./components/ImproveSkills";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className="container main">
         <HeroSection />
+        <ImproveSkills />
       </div>
     </div>
   );
