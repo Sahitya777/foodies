5be70884056433115a7134d4cd200cfbb3224f25
Sahitya77 <sahityanijhawan@gmail.com>
2022-04-05 22:54:09 +0530

    front page done!!

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HeroSection from "./components/HeroSection";
 import ImproveSkills from "./components/ImproveSkills";
 import QuoteSection from "./components/QuoteSection";
 import ChefSection from "./components/ChefSection";
+import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
@@ -13,6 +14,7 @@ function App() {
         <QuoteSection />
         <ChefSection />
       </div>
+      <Footer/>
     </div>
   );
 }
