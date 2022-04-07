be74beda75f1dc5be1ea629368107f4d4d849015
Sahitya77 <sahityanijhawan@gmail.com>
2022-04-07 19:38:18 +0530

    recipe section complete!!!

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,17 +1,79 @@
+import PreviousSearches from "../components/PreviousSearches"
+import RecipeCard from "../components/RecipeCard"
 
+export default function Recipes(){
+    const recipes = [
+        {
+            title: "Chicken Pan Pizza",
+            image: "/img/gallery/img_1.jpg",
+            authorImg: "/img/top-chiefs/img_1.jpg",
+        }, 
+        {
+            title: "Spaghetti and Meatballs",
+            image: "/img/gallery/img_4.jpg",
+            authorImg: "/img/top-chiefs/img_2.jpg",
+        },
+        {
+            title: "American Cheese Burger",
+            image: "/img/gallery/img_5.jpg",
+            authorImg: "/img/top-chiefs/img_3.jpg",
+        },
+        {
+            title: "Mutton Biriyani",
+            image: "/img/gallery/img_6.jpg",
+            authorImg: "/img/top-chiefs/img_5.jpg",
+        },
+        {
+            title: "Japanese Sushi",
+            image: "/img/gallery/img_10.jpg",
+            authorImg: "/img/top-chiefs/img_6.jpg",
+        },
+        {
+            title: "Chicken Pan Pizza",
+            image: "/img/gallery/img_1.jpg",
+            authorImg: "/img/top-chiefs/img_1.jpg",
+        }, 
+        {
+            title: "Spaghetti and Meatballs",
+            image: "/img/gallery/img_4.jpg",
+            authorImg: "/img/top-chiefs/img_2.jpg",
+        },
+        {
+            title: "American Cheese Burger",
+            image: "/img/gallery/img_5.jpg",
+            authorImg: "/img/top-chiefs/img_3.jpg",
+        },
+        {
+            title: "Mutton Biriyani",
+            image: "/img/gallery/img_6.jpg",
+            authorImg: "/img/top-chiefs/img_5.jpg",
+        },
+        {
+            title: "Japanese Sushi",
+            image: "/img/gallery/img_10.jpg",
+            authorImg: "/img/top-chiefs/img_6.jpg",
+        },
+        {
+            title: "American Cheese Burger",
+            image: "/img/gallery/img_5.jpg",
+            authorImg: "/img/top-chiefs/img_3.jpg",
+        },
+        {
+            title: "Mutton Biriyani",
+            image: "/img/gallery/img_6.jpg",
+            authorImg: "/img/top-chiefs/img_5.jpg",
+        }
+    ].sort(() => Math.random() - 0.5)
 
-function Recipes() {
-  const searches=['pizza','burger','cookies','juice','biryani','salad','ice cream','pudding','soup']
-  return (
-    <div className="previous-searches section">
-      <h2>Previous Searches</h2>
-      <div className="previous-searches-container">
-      {searches.map(search => (<div  className="search-items">
-        {search}
-      </div>))}
-      </div>
-    </div>
-  )
+    return (
+        <div>
+            <PreviousSearches />
+            <div className="recipes-container">
+                {/* <RecipeCard /> */}
+                {recipes.map((recipe, index) => (
+                    <RecipeCard key={index} recipe={recipe} />
+                ))}
+            </div>
+        </div>
+    )
 }
-
-export default Recipes;
