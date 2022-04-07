f20a3c760235d85e00d5b33aa4d0ced9de688352
Sahitya77 <sahityanijhawan@gmail.com>
2022-04-07 15:32:54 +0530

    recipes js updated!!

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,9 +1,15 @@
 
 
 function Recipes() {
+  const searches=['pizza','burger','cookies','juice','biryani','salad','ice cream','pudding','soup']
   return (
-    <div>
-      Recipes
+    <div className="previous-searches section">
+      <h2>Previous Searches</h2>
+      <div className="previous-searches-container">
+      {searches.map(search => (<div  className="search-items">
+        {search}
+      </div>))}
+      </div>
     </div>
   )
 }
